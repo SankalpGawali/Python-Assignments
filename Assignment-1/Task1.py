b95c88c66f4b489eb40645601346f235d922078a
SankalpGawali <165922115+SankalpGawali@users.noreply.github.com>
2026-01-16 14:31:36 +0530

    Add files via upload

---
+++ Assignment-1/Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number "))
+b=int(input("Enter a number "))
+c=a+b
+print("Addition : ",c)
+d=a-b
+print("Subtraction : ",d)
+e=a*b
+print("Multiplication : ",e)
+f=a/b
+print("Division : ",round(f,2))
